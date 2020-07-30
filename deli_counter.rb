106360e8da8ddd802eece1d21f5974728ddfb7c2
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-30 20:05:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,15 @@ katz_deli=[]
 
 def line(katz_deli) 
   
-  line_position= []
+  other_line= []
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     number= 1 
     katz_deli.each do |customer|
-      line_position << "#{number}. #{customer}"
+      other_line << "#{number}. #{customer}"
       number +=1 
     end 
-    puts "The line is currently: #{line_positions.join(" ")}"
+    puts "The line is currently: #{other_line.join(" ")}"
    end
 end 
